18144c0691a25dfbc03c8b5f07e3a03b1c0d65c1
Eddy Caldas <eddycaldas@live.com>
2018-02-08 10:48:04 -0700

    remove node modules

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
-const knex = require('knex')
+const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')
-const env = process.env.NODE_ENV || 'development'
-
-module.exports = knex(config[env])
-
+const environmentConfig = config[environment];
+const knex = require('knex');
+const connection = knex(environmentConfig);
+module.exports = connection;
