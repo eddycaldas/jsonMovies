83e7dfe31a69b3cf6813bacf7625b8361fc29b74
Eddy Caldas <eddycaldas@live.com>
2018-02-08 15:21:23 -0700

    env

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
     client: 'pg',
@@ -5,6 +7,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL+'?ssl=true',
   }
 };
