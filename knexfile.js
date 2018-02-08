f35ceaa182a39f5fbda86c3504c59987408edb38
Eddy Caldas <eddycaldas@live.com>
2018-02-08 11:00:13 -0700

    knexfile update

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/jsonMovies',
+    connection: 'postgresql://localhost/jsonMovies',
   },
   production: {
     client: 'pg',
