eadb8faf1d0e8cf7cc9e88fa5ceb8daf43abc696
Eddy Caldas <eddycaldas@live.com>
2018-02-08 10:24:49 -0700

    update express

--- routes/movie.js
+++ routes/movie.js
@@ -1,4 +1,5 @@
-const router = require("Express").Router()
+const express = require('express')
+const router = express.Router()
 const queries = require('../db/queries')
 
 router.get('/', (req, res) => {
